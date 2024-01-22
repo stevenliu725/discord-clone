523bc98db4a3854a693d87224e75ea112ac56e0e
stevenliu725 <stevenliu725@gmail.com>
2024-01-21 19:31:54 -0500

    first commit

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+    return ( 
+    <div>
+        registering
+    </div> 
+    );
+}
+ 
+export default RegisterPage;
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div> 
+            this is a proteted route
+        </div>
+    )
+}
